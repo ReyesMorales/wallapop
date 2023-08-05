0f0c13167778e854812d678f8dd3da5792cdc3d6
Ivarsky <iamh90@gmail.com>
2023-08-05 20:48:08 +0000

    chore: esquema de anuncio

--- backend/models/anuncio.js
+++ backend/models/anuncio.js
@@ -1,14 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Definir el esquema del modelo de Anuncio
 const anuncioSchema = new mongoose.Schema({
-  titulo: { type: String, required: true },
-  descripcion: { type: String, required: true },
-  precio: { type: Number, required: true },
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
+  sell: { type: Boolean, required: true },
+  tags: { type: [String], required: true },
+  photo: String,
 });
 
 // Crear el modelo 'Anuncio' a partir del esquema
-const Anuncio = mongoose.model('Anuncio', anuncioSchema);
-
+const Anuncio = mongoose.model("Anuncio", anuncioSchema);
 
 module.exports = Anuncio;
