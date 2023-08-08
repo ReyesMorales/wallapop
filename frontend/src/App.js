60f8420a6b4e96f9b3ec8db6b94199895c6efb18
Ivarsky <iamh90@gmail.com>
2023-08-08 20:54:45 +0000

    feat: conexion con back y mostrado de datos en el front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App">Hello ducks!</div>;
+  const [anuncios, setAnuncios] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:4000/api/anuncios").then((response) => {
+      setAnuncios(response.data.results);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Hello ducks!</h1>
+      <ul>
+        {anuncios.map((anuncio) => (
+          <li key={anuncio.id}>
+            {anuncio.name}
+            {anuncio.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
