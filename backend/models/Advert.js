ee0cddc89e008f70bfbd713b2a8b98627dcf4559
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-08-24 22:51:45 +0100

    actualizar modelo añadir tags

--- backend/models/Advert.js
+++ backend/models/Advert.js
@@ -1,26 +1,17 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
 
 // Definir el esquema del modelo de Anuncio
-//TODO: tags
 const advertSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  type: { type: String, enum: ["compra", "venta"], required: true },
+  name: { type: String, required: true },
   price: { type: Number, required: true },
-  photo: {
-    type: String,
-    // validate: {
-    //   validator: (value) => {
-    // Utilizamos el método isURL de la librería validator para verificar que la foto sea una URL válida
-    // return validator.isURL(value);
-    //   },
-    //   message: 'La foto debe ser una URL válida',
-    // },
-  },
+  description: { type: String, required: true },
+  type: { type: String, enum: ['compra', 'venta'], required: true },
+  tags: { type: [String], required: true },
+  photo: { type: String }, 
 });
 
 // Crear el modelo 'Anuncio' a partir del esquema
-const Advert = mongoose.model("Advert", advertSchema);
+const Advert = mongoose.model("adverts", advertSchema);
 
+//exportar el modelo
 module.exports = Advert;
