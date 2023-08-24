cc60042df2876eeb01f6593231266938b4d02f99
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-08-24 23:09:48 +0100

    actualizar codigo

--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -1,22 +1,30 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import { Button, Col, Container, Nav, Navbar } from "react-bootstrap";
+import { ReactComponent as Logo } from "../../assets/duck-icon.svg";
+import { Link } from "react-router-dom";
 
-function Cabecera() {
+function Header() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="dark" data-bs-theme="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Nodepop React</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <Logo /> What a Duck!
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav>
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/anuncios">Anuncios</Nav.Link>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Quienes somos</Nav.Link>
+            <Nav.Link href="/adverts">Anuncios</Nav.Link>
           </Nav>
+          <Col xs="auto" className="ms-auto">
+            <Link to="/login">
+              <Button variant="secondary">Registrarte o Iniciar sesión</Button>
+            </Link>
+          </Col>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 }
 
-export default Cabecera;
+export default Header;
+
