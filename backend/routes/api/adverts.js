4b2af2dd185d1d37f46b61b6934572859240efe3
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-08-27 12:48:16 +0100

    añadir ruta para eliminar anuncio

--- backend/routes/api/adverts.js
+++ backend/routes/api/adverts.js
@@ -92,5 +92,25 @@ router.put("/edit/:id", async (req, res, next) => {
   }
 });
 
+// Ruta para eliminar un anuncio por ID
+router.delete('/:id', async (req, res, next) => {
+  const id = req.params.id;
+  
+  try {
+      const result = await Advert.findByIdAndDelete(id);
+      
+      // Si no se encuentra el anuncio, devolvemos un 404
+      if (!result) {
+          return res.status(404).json({ message: 'Anuncio no encontrado' });
+      }
+
+      // Si la eliminación es exitosa, devolvemos un 204 sin contenido
+      res.status(204).end();
+  } catch (error) {
+      next(error);
+  }
+});
+
+
 
 module.exports = router;
