ca4fa27521c64dabfc5a318aef82c27f6d80ce96
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-08-31 20:35:24 +0100

    eliminar multer

--- backend/app.js
+++ backend/app.js
@@ -15,26 +15,6 @@ require("./lib/connectMongoose");
 
 var app = express();
 
-const multer = require("multer");
-
-// Configura Multer para guardar archivos en una carpeta específica
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public/uploads"); // Carpeta donde se guardarán los archivos
-  },
-  filename: function (req, file, cb) {
-    // Genera un nombre de archivo único basado en la fecha actual
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage });
-
-
-// Usa multer como middleware para manejar las solicitudes que requieren análisis de archivos
-app.use(upload.single("photo")); 
-
-
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -45,6 +25,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/uploads/:imageName', (req, res) => {
+  console.log("Dentro de la ruta de imágenes"); // Nuevo log
+
+  const imageName = req.params.imageName;
+  const ext = path.extname(imageName);
+
+  if (ext === '.jpeg' || ext === '.jpg') {
+      res.setHeader('Content-Type', 'image/jpeg');
+  } else if (ext === '.png') {
+      res.setHeader('Content-Type', 'image/png');
+  }
+  console.log("Establecido el Content-Type a:", res.get('Content-Type')); // Nuevo log
+
+  res.sendFile(path.join(__dirname, 'public/uploads', imageName));
+});
+
 
 /**
  * Rutas del API
@@ -63,15 +60,15 @@ app.use(function (req, res, next) {
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+// app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto ${port}`);
