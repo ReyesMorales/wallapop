cc6c9ab989fea25eff546baed44cb4181920525b
Diego! <91294213+Kabash87@users.noreply.github.com>
2023-09-01 13:21:29 -0500

    Registro de usuarios funcional

---
+++ backend/models/Advert.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+// Definir el esquema del modelo de Anuncio
+const advertSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String, required: true },
+  type: { type: String, enum: ['compra', 'venta'], required: true },
+  tags: { type: [String], required: true },
+  photo: { type: String }, 
+});
+
+// Crear el modelo 'Anuncio' a partir del esquema
+const Advert = mongoose.model("adverts", advertSchema);
+
+//exportar el modelo
+module.exports = Advert;
