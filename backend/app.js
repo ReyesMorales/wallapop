2be58dd8b6f74fb506081bfda53553dce28cb2da
Ivarsky <iamh90@gmail.com>
2023-09-01 18:23:35 +0000

    bug: ports

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,6 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var port = process.env.PORT;
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -25,22 +24,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/uploads/:imageName', (req, res) => {
+app.get("/uploads/:imageName", (req, res) => {
   console.log("Dentro de la ruta de imágenes"); // Nuevo log
 
   const imageName = req.params.imageName;
   const ext = path.extname(imageName);
 
-  if (ext === '.jpeg' || ext === '.jpg') {
-      res.setHeader('Content-Type', 'image/jpeg');
-  } else if (ext === '.png') {
-      res.setHeader('Content-Type', 'image/png');
+  if (ext === ".jpeg" || ext === ".jpg") {
+    res.setHeader("Content-Type", "image/jpeg");
+  } else if (ext === ".png") {
+    res.setHeader("Content-Type", "image/png");
   }
-  console.log("Establecido el Content-Type a:", res.get('Content-Type')); // Nuevo log
+  console.log("Establecido el Content-Type a:", res.get("Content-Type")); // Nuevo log
 
-  res.sendFile(path.join(__dirname, 'public/uploads', imageName));
+  res.sendFile(path.join(__dirname, "public/uploads", imageName));
 });
-
 
 /**
  * Rutas del API
@@ -60,17 +58,13 @@ app.use(function (req, res, next) {
 
 // error handler
 // app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  // res.locals.message = err.message;
-  // res.locals.error = req.app.get("env") === "development" ? err : {};
+// set locals, only providing error in development
+// res.locals.message = err.message;
+// res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
+// render the error page
 //   res.status(err.status || 500);
 //   res.render("error");
 // });
-
-app.listen(port, () => {
-  console.log(`Servidor escuchando en el puerto ${port}`);
-});
 
 module.exports = app;
