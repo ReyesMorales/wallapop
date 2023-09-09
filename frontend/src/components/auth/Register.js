f5ce88f2cff9bfd30100f23a601ef253c9d45aa9
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-09-09 18:56:38 +0100

    añadir Layout

--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -10,6 +10,7 @@ import {
 } from "react-bootstrap";
 import { createUser } from "./service";
 import { Link } from "react-router-dom";
+import Layout from "../Layout/Layout";
 
 function CreateRegister() {
   const [name, setName] = useState("");
@@ -102,126 +103,132 @@ function CreateRegister() {
   // Lógica para manejar el envío del formulario y crear el usuario
 
   return (
-    <Container>
-      <Row className="justify-content-md-center">
-        <Col md="6">
-          {/*Modal de confirmación */}
-          <Modal show={showModal} onHide={handleCancel}>
-            <Modal.Header closeButton>
-              <Modal.Title>Confirmar</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              ¿Estás seguro de que deseas crear el usuario?
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={handleCancel}>
-                Cancelar
+    <Layout title="Registro">
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col md="6">
+            {/*Modal de confirmación */}
+            <Modal show={showModal} onHide={handleCancel}>
+              <Modal.Header closeButton>
+                <Modal.Title>Confirmar</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                ¿Estás seguro de que deseas crear el usuario?
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleCancel}>
+                  Cancelar
+                </Button>
+                <Button variant="primary" onClick={handleConfirm}>
+                  Aceptar
+                </Button>
+              </Modal.Footer>
+            </Modal>
+
+            {/*Alert para mostrar el mensaje de éxito */}
+            {successMessage && (
+              <Alert variant="success">{successMessage}</Alert>
+            )}
+            {/*Alert para mostrar el mensaje de error */}
+            {errorMessage && <Alert variant="danger">{errorMessage}</Alert>}
+            <Form onSubmit={handleSubmit} encType="multipart/form-data">
+              <br />
+              <h2>Registrate gratis</h2>
+              <p>No te pierdas de todas las cosas nuevas que tenemos para ti</p>
+              <br />
+
+              <Form.Group controlId="formName">
+                <Form.Label>Nombre y Apellido:</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="¿Como te llamas?"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                {formErrors.name && (
+                  <Form.Text className="text-danger">
+                    {formErrors.name}
+                  </Form.Text>
+                )}
+              </Form.Group>
+
+              <Form.Group controlId="formGender">
+                <Form.Label>¿Cual es tu género?</Form.Label>
+                <Form.Control
+                  as="select"
+                  value={gender}
+                  onChange={(e) => setGender(e.target.value)}
+                >
+                  <option value="" disabled>
+                    - - - Selecciona tu género - - -
+                  </option>
+                  <option value="hombre">Hombre</option>
+                  <option value="mujer">Mujer</option>
+                </Form.Control>
+              </Form.Group>
+
+              <Form.Group controlId="formEmail">
+                <Form.Label>Correo Electronico:</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Ingresa tu correo electronico"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                {formErrors.email && (
+                  <Form.Text className="text-danger">
+                    {formErrors.email}
+                  </Form.Text>
+                )}
+              </Form.Group>
+
+              <Form.Group controlId="formPrice">
+                <Form.Label>Número de telefono:</Form.Label>
+                <Form.Control
+                  type="tel"
+                  placeholder="Ingresa tu numero de contacto"
+                  value={number}
+                  onChange={(e) => setNumber(e.target.value)}
+                />
+                {formErrors.number && (
+                  <Form.Text className="text-danger">
+                    {formErrors.number}
+                  </Form.Text>
+                )}
+              </Form.Group>
+
+              <Form.Group controlId="formPassword">
+                <Form.Label>Contraseña:</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Ingrese su contraseña"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                {formErrors.password && (
+                  <Form.Text className="text-danger">
+                    {formErrors.password}
+                  </Form.Text>
+                )}
+              </Form.Group>
+
+              <br />
+              <Button variant="primary" type="submit">
+                Registrarse
               </Button>
-              <Button variant="primary" onClick={handleConfirm}>
-                Aceptar
-              </Button>
-            </Modal.Footer>
-          </Modal>
-
-          {/*Alert para mostrar el mensaje de éxito */}
-          {successMessage && <Alert variant="success">{successMessage}</Alert>}
-          {/*Alert para mostrar el mensaje de error */}
-          {errorMessage && <Alert variant="danger">{errorMessage}</Alert>}
-          <Form onSubmit={handleSubmit} encType="multipart/form-data">
-            <br />
-            <h2>Registrate gratis</h2>
-            <p>No te pierdas de todas las cosas nuevas que tenemos para ti</p>
-            <br />
-
-            <Form.Group controlId="formName">
-              <Form.Label>Nombre y Apellido:</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="¿Como te llamas?"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-              {formErrors.name && (
-                <Form.Text className="text-danger">{formErrors.name}</Form.Text>
-              )}
-            </Form.Group>
-
-            <Form.Group controlId="formGender">
-              <Form.Label>¿Cual es tu género?</Form.Label>
-              <Form.Control
-                as="select"
-                value={gender}
-                onChange={(e) => setGender(e.target.value)}
-              >
-                <option value="" disabled>
-                  - - - Selecciona tu género - - -
-                </option>
-                <option value="hombre">Hombre</option>
-                <option value="mujer">Mujer</option>
-              </Form.Control>
-            </Form.Group>
-
-            <Form.Group controlId="formEmail">
-              <Form.Label>Correo Electronico:</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Ingresa tu correo electronico"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              {formErrors.email && (
-                <Form.Text className="text-danger">
-                  {formErrors.email}
-                </Form.Text>
-              )}
-            </Form.Group>
-
-            <Form.Group controlId="formPrice">
-              <Form.Label>Número de telefono:</Form.Label>
-              <Form.Control
-                type="tel"
-                placeholder="Ingresa tu numero de contacto"
-                value={number}
-                onChange={(e) => setNumber(e.target.value)}
-              />
-              {formErrors.number && (
-                <Form.Text className="text-danger">
-                  {formErrors.number}
-                </Form.Text>
-              )}
-            </Form.Group>
-
-            <Form.Group controlId="formPassword">
-              <Form.Label>Contraseña:</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Ingrese su contraseña"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              {formErrors.password && (
-                <Form.Text className="text-danger">
-                  {formErrors.password}
-                </Form.Text>
-              )}
-            </Form.Group>
-
-            <br />
-            <Button variant="primary" type="submit">
-              Registrarse
-            </Button>
-            <br />
-            <br />
-            <p>
-              ¿Ya tienes una cuenta creada?{" "}
-              <Link to="/login" style={{ textDecoration: "none" }}>
-                Inicia Sesión
-              </Link>
-            </p>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+              <br />
+              <br />
+              <p>
+                ¿Ya tienes una cuenta creada?{" "}
+                <Link to="/login" style={{ textDecoration: "none" }}>
+                  Inicia Sesión
+                </Link>
+              </p>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </Layout>
   );
 }
 
