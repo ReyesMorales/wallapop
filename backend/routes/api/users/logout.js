b5882cacaca2f0603eb90e5bf0d39f0ea1e182a5
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-09-10 23:34:52 +0100

    crear archivo para ruta del logout

---
+++ backend/routes/api/users/logout.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var router = express.Router();
+
+//Cerrar Sesion
+router.get("/", (req, res) => {
+    res.clearCookie("access-token");
+    res.clearCookie("email-user");
+    res.clearCookie("user-name");
+    res.redirect("http://localhost:3000/login");
+  });
+
+module.exports = router;
