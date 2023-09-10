0fd4f894c19ae8b85e3d177b608afa87e630313f
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-09-10 23:33:59 +0100

    crear index para las rutas de anuncios

---
+++ backend/routes/api/adverts/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+
+router.use("/create-advert", require("./createAdvert"));
+router.use("/edit/", require("./editAdvert"));
+router.get("/:id", require("./getAdvertById"));
+router.delete("/:id", require("./deleteAdvert"));
+router.use("/", require("./getAdverts"));
+
+
+module.exports = router;
