4d50e745a51d10173e01f094f942ff0237b7d5a3
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-09-14 23:40:34 +0100

    cambiar boton login/logout

--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -1,8 +1,43 @@
 import { Button, Col, Container, Nav, Navbar } from "react-bootstrap";
 import { ReactComponent as Logo } from "../../assets/duck-icon.svg";
 import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+
 
 function Header() {
+  const [isLogged, setIsLogged] = useState(false);
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    setIsLogged(false);
+    window.location.href = '/';
+  };
+
+  useEffect(() => {
+    const handleStorageChange = () => {
+        const token = localStorage.getItem('token');
+        const userId = localStorage.getItem('userId');
+        
+        if (token && userId) {
+            setIsLogged(true);
+        } else {
+            setIsLogged(false);
+        }
+    };
+
+    // Iniciar escuchando los cambios
+    window.addEventListener('storage', handleStorageChange);
+
+    // Llamar a la función una vez al inicio para establecer el estado inicial
+    handleStorageChange();
+
+    // Limpiar el listener cuando el componente se desmonte
+    return () => window.removeEventListener('storage', handleStorageChange);
+}, []);
+
+
+
   //TODO: hacer que el boton de registrar o iniciar sesion sea de desloguear cuando se onlogin
   return (
     <Navbar bg="dark" data-bs-theme="dark" expand="lg">
@@ -17,9 +52,13 @@ function Header() {
             <Nav.Link href="/adverts">Anuncios</Nav.Link>
           </Nav>
           <Col xs="auto" className="ms-auto">
-            <Link to="/login">
+          {isLogged ? (
+           <Button variant="secondary" onClick={handleLogout}>Logout</Button>
+            ) : (
+              <Link to="/login">
               <Button variant="secondary">Registrarte o Iniciar sesión</Button>
-            </Link>
+              </Link>
+            )}
           </Col>
         </Navbar.Collapse>
       </Container>
