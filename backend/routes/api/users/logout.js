aa1d62ce315d8049205207ab4a334432bbf32c58
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-09-18 21:55:56 +0100

    añadir redireccion con variable de entorno

--- backend/routes/api/users/logout.js
+++ backend/routes/api/users/logout.js
@@ -6,7 +6,7 @@ router.get("/", (req, res) => {
     res.clearCookie("access-token");
     res.clearCookie("email-user");
     res.clearCookie("user-name");
-    res.redirect("http://localhost:3000/login");
+    res.redirect(`${process.env.FRONTEND_URL}/login`);
   });
 
 module.exports = router;
