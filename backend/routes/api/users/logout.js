7c6bcce9c5ef4faf7cb3646ed35164a358466988
ReyesMorales <reyesmoralesdominguez@gmail.com>
2023-09-18 22:48:35 +0100

    cambiar a metodo post

--- backend/routes/api/users/logout.js
+++ backend/routes/api/users/logout.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 //Cerrar Sesion
-router.get("/", (req, res) => {
+router.post("/", (req, res) => {
     res.clearCookie("access-token");
     res.clearCookie("email-user");
     res.clearCookie("user-name");
@@ -10,3 +10,18 @@ router.get("/", (req, res) => {
   });
 
 module.exports = router;
+
+
+
+// 3. **Configurar variables de entorno en la instancia AWS**:
+//    Una mejor práctica sería configurar las variables de entorno directamente en tu instancia AWS.
+
+//    - Conéctate a tu instancia AWS mediante SSH.
+//    - Establece las variables de entorno. Puedes hacerlo agregándolas al archivo `~/.bashrc` o `~/.bash_profile` (para el usuario con el que se ejecuta la aplicación).
+//      ```bash
+//      echo "export FRONTEND_URL=https://tudominio.com" >> ~/.bashrc
+//      source ~/.bashrc
+//      ```
+
+
+
