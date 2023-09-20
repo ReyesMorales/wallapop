45860f0fa16ec3fc92f49806d687002e3aa70637
Ivarsky <iamh90@gmail.com>
2023-09-20 23:36:09 +0200

    CORS

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var port = process.env.PORT;
+var cors = require("cors");
 
 var indexRouter = require("./routes/index");
 
@@ -14,26 +15,21 @@ require("./lib/connectMongoose");
 
 var app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-
 /**
  * Rutas del Website
  */
 app.use("/", indexRouter);
 
-
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
-
-
-
 
 module.exports = app;
